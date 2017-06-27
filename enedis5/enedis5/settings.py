02fd36ce8fa3a9640032d42bf69dd65cf3bbf1fa
Tibau <xiliar1888@hotmail.fr>
2017-06-27 11:55:58 +0200

    Remove database informations from settings

--- enedis5/enedis5/settings.py
+++ enedis5/enedis5/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import site_settings.py
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -73,12 +74,7 @@ WSGI_APPLICATION = 'enedis5.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
 
 
 # Password validation
